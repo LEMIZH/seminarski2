7bcd5dedbfe9b83b1e38f50af5abec66672b64e9
LEMIZH <77200906+LEMIZH@users.noreply.github.com>
2021-01-09 16:42:44 +0100

    Направио сам свој калкулатор у Java програмском језику, користећи IntelliJ окружење.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 0;
+        int b = 0;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanObject = new Scanner(System.in);
+        System.out.println("Унесите први број: ");
+        a = scanObject.nextInt();
+        System.out.println("Унесите други број: ");
+        b = scanObject.nextInt();
+        System.out.println("Одаберите операцију: +, -, *, /");
+        operator = scanObject.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                answer = a + b;
+                break;
+            case '-':
+                answer = a - b;
+                break;
+            case '*':
+                answer = a * b;
+                break;
+            case '/':
+                answer = a % b;
+                break;
+        }
+
+        System.out.println(a + " " + operator + " " + b + " = " + answer);
     }
 }
